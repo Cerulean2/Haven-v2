6bbc5ede3effde9a2e57048ad53ade50863044c3
Ceru <andrew.p.glidden@gmail.com>
2026-05-30 16:31:36 -0400

    Add exception handling for cog loading and set bot presence

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import sys
 
 from discord.ext import commands
 from dotenv import load_dotenv
+from logger import logger
 
 load_dotenv()
 
@@ -14,16 +15,26 @@ class HavenBot(commands.Bot):
     async def setup_hook(self):
         for file in os.listdir(path="Commands/General"):
             if file.endswith(".py"):
-                await self.load_extension(f"Commands.General.{file[:-3]}")
-                print(f"Loaded Cog {file}")
+                try:
+                    await self.load_extension(f"Commands.General.{file[:-3]}")
+                    logger.info(f"Loaded Cog {file}")
+                except Exception as e:
+                    logger.error(f"Failed to log Cog {file[:-3]}: {e}")
     
     async def on_ready(self):
-        print(f"Logged into Discord as {self.user} ({self.application_id})")
+        logger.info(f"Logged into Discord as {self.user} ({self.application_id})")
+        
+        status = discord.Activity(
+            type=discord.ActivityType.playing,
+            name=f"{len(self.guilds)} guilds | h.help"
+            )
+        
+        await self.change_presence(status=discord.Status.online, activity=status)
 
 bot = HavenBot(
     command_prefix='h.',
     help_command=None,
-    intents=intents
+    intents=intents,
 )
 
 token = os.getenv("DISCORD_TOKEN")
