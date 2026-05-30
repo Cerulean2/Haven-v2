27511a0cdf2396c977e4c8c4f476550f2d4512e2
Ceru <andrew.p.glidden@gmail.com>
2026-05-30 16:32:27 -0400

    Rename class in ping cog to Ping

--- Commands/General/ping.py
+++ Commands/General/ping.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-class General(commands.Cog):
+class Ping(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
@@ -11,4 +11,4 @@ class General(commands.Cog):
         await ctx.send("Pong! `{0}ms`".format(round(self.bot.latency * 1000)))
 
 async def setup(bot):
-    await bot.add_cog(General(bot))
+    await bot.add_cog(Ping(bot))
