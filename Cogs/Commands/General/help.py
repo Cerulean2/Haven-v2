02ca3e03768b31eaecec6fa230eda716f900b867
Ceru <andrew.p.glidden@gmail.com>
2026-06-21 19:21:39 -0400

    Update help command to be actually useful

--- Cogs/Commands/General/help.py
+++ Cogs/Commands/General/help.py
@@ -10,7 +10,30 @@ class Help(commands.Cog):
         """Help message"""
         config = self.bot.config
         
-        await ctx.reply(f"We're out of help right now. Try using a command like `{config['prefix']}ban` for usage.")
+        embed = discord.Embed(title="────────────────────────────────────────",
+                      colour=0x0080c0)
+
+        embed.set_author(name="Haven Commands",
+                        icon_url="https://cdn.discordapp.com/avatars/1506465698124267552/a831a42e05a844a560846c0de81fe9bf.png?size=4096")
+        
+        embed.add_field(name="**General**",
+                        value=f"""
+                        `{config['prefix']}help` - Shows this command.
+                        \n`{config['prefix']}ping` - Returns the bots latency.
+                        \n`{config['prefix']}cat` - Sends a random cat gif.
+                        \n`{config['prefix']}snake` - Sssssss....
+                        \n`{config['prefix']}snipe` - Snipes a recently deleted message.
+                        """,
+                        inline=True)
+        
+        embed.add_field(name="**Moderation**",
+                        value=f"""
+                        `{config['prefix']}ban` - Bans a member from the guild.
+                        \n`{config['prefix']}kick` - Kicks a member from the guild.
+                        """,
+                        inline=True)
+        
+        await ctx.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(Help(bot))
